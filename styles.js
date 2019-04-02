609150e89e59bfd2c83938b26b991ca661d2ebe8
Tyrell100 <benjamingachara@gmail.com>
2019-04-02 17:53:59 +0300

    After finishing the functionality of the application

--- styles.js
+++ styles.js
@@ -1,7 +1,9 @@
 function check(){
-  var score=parseInt(document.getElementsByClassName('form-control').value)
-var finalScore=0
-if (score=="20"){
-  finalScore+=20
-alert(finalScore)
+  var score1=parseInt(document.getElementById('question1').value)
+  var score2=parseInt(document.getElementById('question2').value)
+  var score3=parseInt(document.getElementById('question3').value)
+  var score4=parseInt(document.getElementById('question4').value)
+  var score5=parseInt(document.getElementById('question5').value)
+  var total=score1+score2+score3+score4+score5
+  alert("You got" +total+"%"+"of the questions right!")
 }
